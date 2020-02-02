3523e074b6a0f63f72754aaaff6eb2f88e38af9c
Collin Nolen <collinnolen@gmail.com>
2020-02-02 13:37:05 -0700

    Initial file saving feature added to the script.

--- TemperatureHumidityReading/dh22.py
+++ TemperatureHumidityReading/dh22.py
@@ -1,18 +1,92 @@
+import getopt
+import sys
 import time
- 
+from datetime import date
+
+# Sensor imports
 import adafruit_dht
 import board
- 
+
+# read commandline arguments, first
+fullCmdArguments = sys.argv
+
+# - further arguments
+argumentList = fullCmdArguments[1:]
+
+# prepare the valid parameters
+unixOptions = "ho:v"
+gnuOptions = ["help", "toFile=", "interval=", "directory="]
+
+# parse the arguments
+try:
+    arguments, values = getopt.getopt(argumentList, unixOptions, gnuOptions)
+except getopt.error as err:
+    # output error, and return with an error code
+    print(str(err))
+    sys.exit(2)
+
+# set some paramter defaults
+toFile = True
+interval = 10
+directory = r"/media/usb/"
+
+# Parsing the current argument's values
+for currentArgument, currentValue in arguments:
+    # HELP
+    if currentArgument in ("-h", "--help"):
+        print(
+            "INTERVAL: The sleep time in seconds between readings. Defaults to 10 seconds.")
+        print("TOFILE: TRUE/FALSE option to either print results to file or console. Defaults to TRUE")
+        print("DIRECTORY: Location to save files. Defaults to /media/usb/")
+    # DIRECTORY
+    elif currentArgument in ("-d", "--directory"):
+        print(("Using directory: %s.") % (currentValue))
+        directory = currentValue
+    # INTERVAL
+    elif currentArgument in ("-i", "--interval"):
+        print(("Using interval of %s seconds") % (currentValue))
+        interval = currentArgument
+    # TOFILE
+    elif currentArgument in ("-tf", "--ToFile"):
+        # TRUE
+        if(currentValue in ("t", "T", "TRUE", "true")):
+            print("Writing results to file")
+            toFile = True
+        # FALSE
+        elif(currentValue in ("f", "F", "FALSE", "false")):
+            toFile = False
+            print("Writing results to terminal")
+        # OTHER
+        else:
+            print(
+                "Invalid toFile option. Use one of these options: t, f, true, false, TRUE, FALSE")
+            sys.exit(2)
+
+# Load the sensor
 dht = adafruit_dht.DHT22(board.D4)
- 
+
+# main loop
 while True:
     try:
         temperature = dht.temperature
         humidity = dht.humidity
-        # Print what we got to the REPL
-        print("Temp: {:.1f} *C \t Humidity: {}%".format(temperature, humidity))
+
+        if(toFile):
+            fileName = date.today().strftime('%m-%d-%Y') + ".txt"
+            file = open(directory + fileName, "a")
+            file.Write(
+                "Temp: {:.1f} *F \t Humidity: {}%".format((temperature*1.8), humidity))
+            file.close()
+        else:
+            print(
+                "Temp: {:.1f} *F \t Humidity: {}%".format((temperature*1.8), humidity))
+
     except RuntimeError as e:
         # Reading doesn't always work! Just print error and we'll try again
-        print("Reading from DHT failure: ", e.args)
- 
-    time.sleep(1)
+        if not toFile:
+            print("Reading from DHT failure: ", e.args)
+    except OSError as e:
+        print("Could not open file: %s" % directory)
+        sys.exit(2)
+
+    time.sleep(interval)
